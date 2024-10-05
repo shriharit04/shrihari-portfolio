5486fa2d834eb3029cb3e6be81e512682a9f7e36
Shrihari T <shriharit2004@gmail.com>
2024-10-05 17:06:05 +0530

    made small change to .eslintrc.json to remove warning in build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import {Inter} from "next/font/google";
-import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./Components/main/Navbar";
 
